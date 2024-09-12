1d9c79ab8534c8072855b0479310fe6dc8e3b528
N-jongsik <jongsik0625@naver.com>
2024-09-12 17:35:19 +0900

    map

--- src/components/map/MapComp.js
+++ src/components/map/MapComp.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const { kakao } = window;
 
 function MapComp() {
-  return <div>MapComp</div>;
+  useEffect(() => {
+    const mapContainer = document.getElementById("map"), // 지도를 표시할 div
+      mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+      };
+
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    const map = new kakao.maps.Map(mapContainer, mapOption);
+  }, []);
+  return (
+    <>
+      <div id="map" style={{ width: "100%", height: "500px" }}></div>
+    </>
+  );
 }
 
 export default MapComp;
