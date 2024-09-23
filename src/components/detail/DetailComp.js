9482d30157543dee0328909b6493ac050ce3c60f
hyeji <yhj061987@gmail.com>
2024-09-23 18:00:24 +0900

    feat: DetailComp's review list

--- src/components/detail/DetailComp.js
+++ src/components/detail/DetailComp.js
@@ -72,15 +72,107 @@ const product = {
     { src: map },
   ],
   reviewImage: [
-    { src: review0 },
-    { src: review1 },
-    { src: review2 },
-    { src: review3 },
-    { src: review4 },
-    { src: review5 },
-    { src: review6 },
-    { src: review7 },
+    {
+      src: review0,
+      goods: "아주 좋아요",
+      date: "2023.11.7",
+      content:
+        "파타고니아 너무 마음에 듭니다. 사이즈는 정사이즈 같아요. 재질이 추운날 입기 좋아보입니다",
+      writer: "kickbo*****",
+      height: "160~164cm",
+      weight: "45~49kg",
+      howTo: "일상복",
+      size: "XS",
+      grade: "일반회원",
+    },
+    {
+      src: review1,
+      goods: "아주 좋아요",
+      date: "2024.9.1",
+      content:
+        "73 스카이라인 약간 빈티지 하면서 심플한게 아주 맘에 들어요 파타고니아 화이팅입니다.",
+      writer: "sunhw****",
+      height: "165~169cm",
+      weight: "65~69kg",
+      howTo: "일상복",
+      size: " M",
+      grade: "일반회원",
+    },
+    {
+      src: review2,
+      goods: "아주 좋아요",
+      date: "2023.10.15",
+      content:
+        "미리 겨울 대비해서 구매했습니다 사이즈도 잘맞고 두툼한 편이네요 좋아요",
+      writer: "nto****",
+      height: "175~179cm",
+      weight: "75~79kg",
+      howTo: "일상복",
+      size: " L",
+      grade: "일반회원",
+    },
+    {
+      src: review3,
+      goods: "아주 좋아요",
+      date: "2023.12.18",
+      content:
+        "속에서 털 같은게 다름 옷에 묻어 나와서 어찌해야 할지? 모르겠어요? 한 번 한 시간 정도 입었는데? 바지에 털이 묻어 나와요!!!",
+      writer: "jeongl****",
+      height: "150~154cm",
+      weight: "44kg 이하",
+      howTo: "일상복",
+      size: "XS",
+      grade: "일반회원",
+    },
+    {
+      src: review4,
+      goods: "아주 좋아요",
+      date: "2023.10.24",
+      content:
+        "색상도 그렇고 사이즈도 딱 맞고 잘 입을 거 같습니다 모니터로 보기보다 이쁩니다",
+      writer: "corona1****",
+    },
+    {
+      src: review5,
+      goods: "아주 좋아요",
+      date: "2023.8.27",
+      content:
+        "사이즈 정사이즈 제가 키가 커서 팔이 맞는 제품 찾기가 힘든데 딱이네요^^",
+      writer: "slr1****",
+      height: "185~189cm",
+      weight: "80~84kg",
+      howTo: "일상복",
+      size: "XL",
+      grade: "일반회원",
+    },
+    {
+      src: review6,
+      goods: "아주 좋아요",
+      date: "2023.11.7",
+      content:
+        "73 스카이라인 약간 빈티지 하면서 심플한게 아주 맘에 들어요 파타고니아 화이팅입니다.",
+      writer: "kickbo*****",
+      height: "160~164cm",
+      weight: "45~49kg",
+      howTo: "일상복",
+      size: "XS",
+      grade: "일반회원",
+    },
+    {
+      src: review7,
+      goods: "아주 좋아요",
+      date: "2023.11.7",
+      content:
+        "마감도 깔끔하고 전체적인 퀄리티가 좋아요. 재생소재로 만든 의류라서 완전 흰색은 아니고 은은한 아이보리색인데 그래서 더 맘에 듭니다",
+      writer: "kickbo*****",
+      height: "160~164cm",
+      weight: "65~69kg",
+      howTo: "일상복",
+      size: " M",
+      grade: "일반회원",
+    },
   ],
+
   colors: [
     { name: "Ink Black", class: "bg-black", selectedClass: "ring-gray-950" },
     {
@@ -914,50 +1006,114 @@ function DetailComp() {
               </Menu>
             </div>
           </div>
-          <div className="inline-flex justify-between w-full border-t border-gray-200 items-center h-16">
-            <div className="inline-flex space-x-4 items-center h-full m-0 my-auto ">
-              <div className="flex items-center">
-                {[0, 1, 2, 3, 4].map((rating) => (
-                  <StarIcon
-                    key={rating}
-                    className={classNames(
-                      reviews.average > rating
-                        ? "text-gray-900"
-                        : "text-gray-200",
-                      "h-5 w-5 flex-shrink-0"
-                    )}
-                    aria-hidden="true"
-                  />
-                ))}
-                <div>
-                  <p className="inline-block align-middle ml-2.5 text-sm font-bold">
-                    아주 좋아요
-                  </p>
+          <div>
+            {product.reviewImage.slice(0, 4).map((review, index) => (
+              <div
+                key={index}
+                className="flex justify-between mb-6  border-t border-gray-200"
+              >
+                <div className="items-stretch w-[800px]">
+                  {/* Review header */}
+                  <div className="inline-flex justify-between w-full items-center h-16">
+                    <div className="inline-flex space-x-4 items-center h-full m-0 my-auto">
+                      <div className="flex items-center">
+                        {[0, 1, 2, 3, 4].map((rating) => (
+                          <StarIcon
+                            key={rating}
+                            className={classNames(
+                              reviews.average > rating
+                                ? "text-gray-900"
+                                : "text-gray-200",
+                              "h-5 w-5 flex-shrink-0"
+                            )}
+                            aria-hidden="true"
+                          />
+                        ))}
+                        <div className="flex justify-between">
+                          <p className="inline-block align-middle ml-2.5 text-sm font-bold">
+                            {review.goods}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="inline-flex items-center h-full">
+                      <p className="inline-block align-middle ml-2.5 text-xs text-[#707680]">
+                        {review.date}
+                      </p>
+                    </div>
+                  </div>
+
+                  {/* Review content */}
+                  <div className="flex justify-between">
+                    <div>
+                      <p className="text-[#14161A] text-xs mb-3">
+                        {review.content}
+                      </p>
+                    </div>
+                  </div>
+                  {/* Review image */}
+                  {review.src && (
+                    <div className="mb-2">
+                      <img
+                        src={review.src}
+                        alt="review"
+                        className="w-32 h-32 rounded"
+                      />
+                    </div>
+                  )}
+
+                  {/* Review actions */}
+                  <div className="flex mb-2 pb-4 text-[#707680] text-xs items-center">
+                    <i className="fa-regular fa-thumbs-up"></i>
+                    <p className="px-2">도움돼요 1</p> |{" "}
+                    <p className="px-2">댓글 0</p>
+                  </div>
+                </div>
+
+                {/* Right side box */}
+                <div
+                  className="bg-[#f5f5f5] ml-10 mt-10 mb-5 p-10 rounded-md text-xs text-gray-600 relative flex flex-col justify-center"
+                  style={{ width: "calc(25% - 70px)" }}
+                >
+                  <div className="flex flex-col justify-center">
+                    <p className="mb-4 text-[#14161a] font-bold">
+                      {review.writer}
+                    </p>
+                    <p>의 리뷰입니다.</p>
+                  </div>
+                  {review.height && (
+                    <div className="flex leading-5">
+                      <p className="text-[#707680] mr-1">키</p>
+                      <p className="text-black">{review.height}</p>
+                    </div>
+                  )}
+                  {review.weight && (
+                    <div className="flex leading-5">
+                      <p className="text-[#707680] mr-1">몸무게</p>
+                      <p className="text-black">{review.weight}</p>
+                    </div>
+                  )}
+                  {review.howTo && (
+                    <div className="flex leading-5">
+                      <p className="text-[#707680] mr-1">용도</p>
+                      <p className="text-black">{review.howTo}</p>
+                    </div>
+                  )}
+                  {review.size && (
+                    <div className="flex leading-5">
+                      <p className="text-[#707680] mr-1">평소사이즈</p>
+                      <p className="text-black">{review.size}</p>
+                    </div>
+                  )}
+                  {review.grade && (
+                    <div className="flex leading-5">
+                      <p className="text-[#707680] mr-1">회원 등급</p>
+                      <p className="text-black">{review.grade}</p>
+                    </div>
+                  )}
                 </div>
               </div>
-            </div>
-            <div className="inline-flex items-center h-full">
-              <p className="inline-block align-middle ml-2.5 text-xs text-[#707680]">
-                2023.11.7
-              </p>
-            </div>
-          </div>
-          <div>
-            <p className="text-[#14161A] text-xs mb-3">
-              73 스카이라인 약간 빈티지 하면서 심플한게 아주 맘에 들어요
-              파타고니아 화이팅입니다.
-            </p>
-            <div className="mb-2">
-              <img
-                src={product.reviewImage[0].src}
-                alt="review"
-                className="w-32 h-32 rounded"
-              />
-            </div>
-            <div className=" flex mb-2 text-[#707680]">
-              <i class="fa-regular fa-thumbs-up"></i>
-              <p className="px-2">도움돼요 1</p> | <p> 댓글 0</p>
-            </div>
+            ))}
           </div>
         </div>
       </div>
