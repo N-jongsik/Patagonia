8924c032babeecc978909f4a5502ebbb7b88ccec
N-jongsik <jongsik0625@naver.com>
2024-09-23 14:03:37 +0900

    fix:Store Search

--- src/layout/NavbarComp.js
+++ src/layout/NavbarComp.js
@@ -32,11 +32,11 @@ function NavbarComp() {
                 className="font-bold h-full flex justify-center items-center text-center"
                 style={{ fontSize: "1.2rem", height: "4.8rem" }}
               >
-                추석 연휴 배송 안내
+                {/* 추석 연휴 배송 안내 */}
               </div>
             </h1>
           </nav>
-          <div className="hidden md:flex text-white ">
+          <div className="hidden md:flex text-white pr-8">
             <Link
               to={"/map"}
               className="font-bold flex justify-center items-center h-full text-center"
@@ -55,8 +55,8 @@ function NavbarComp() {
           </div>
         </div>
         <nav>
-          <div className="flex justify-around bg-white">
-            <div className="flex justify-center items-center">
+          <div className="flex justify-between bg-white">
+            <div className="hidden md:flex justify-center items-center pl-10">
               <Link to={"/"}>
                 <img
                   src={require("../assets/images/logo_patagonia.jpg")}
@@ -64,6 +64,14 @@ function NavbarComp() {
                 />
               </Link>
             </div>
+            <ul className="flex justify-center items-center pl-2">
+              <li className="p-2 md:p-5 block md:hidden">
+                <i class="fa-solid fa-bars"></i>
+              </li>
+              <li className="p-2 md:p-5 block md:hidden">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </li>
+            </ul>
             <ul className="before_list flex justify-center items-center">
               <li className="h-full relative flex justify-center items-center">
                 <a
@@ -102,21 +110,31 @@ function NavbarComp() {
                 </a>
               </li>
             </ul>
-            <div className="text-black block py-2">
-              <ul className="flex justify-center items-center">
-                <li className="p-5 hidden md:block">
+            <div className="flex md:hidden justify-center items-center">
+              <Link to={"/"}>
+                <img
+                  src={require("../assets/images/logo_patagonia.jpg")}
+                  alt="logo_patagonia"
+                />
+              </Link>
+            </div>
+            <div className="text-black block py-2 pr-2 md:pr-8">
+              <ul className="flex justify-center items-center cursor-pointer">
+                <li className="p-2 md:p-5 hidden md:block">
                   <i class="fa-solid fa-magnifying-glass"></i>
                 </li>
-                <li className="p-5">
+                <li className="p-2 md:p-5">
                   <i class="fa-regular fa-user"></i>
                 </li>
-                <li className="p-5">
+                <li className="p-2 md:p-5">
                   <i class="fa-regular fa-heart"></i>
                 </li>
-                <li className="p-5">
-                  <i class="fa-solid fa-cart-shopping"></i>
+                <li className="p-2 md:p-5">
+                  <Link to={"/cart"}>
+                    <i class="fa-solid fa-cart-shopping"></i>
+                  </Link>
                 </li>
-                <li className="p-5 hidden md:block">
+                <li className="p-2 md:p-5 hidden md:block">
                   <i class="fa-solid fa-bars"></i>
                 </li>
               </ul>
