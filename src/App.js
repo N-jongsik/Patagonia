3d67640e1d9304949efa30b2c98d262db9c4cb55
N-jongsik <jongsik0625@naver.com>
2024-09-23 21:16:24 +0900

    feat:router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import MapComp from "./components/map/MapComp";
-import LayoutComp from "./layout/LayoutComp";
-import MainComp from "./components/home/MainComp";
-import ShopComp from "./components/shop/ShopComp";
-import DetailComp from "./components/detail/DetailComp";
-import CartComp from "./components/cart/CartComp";
+import { RouterProvider } from "react-router-dom";
+import root from "./router/root";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<LayoutComp />}>
-          <Route index element={<MainComp />} />
-          <Route path="/shop" element={<ShopComp />} />
-          <Route path="/detail" element={<DetailComp />} />
-          <Route path="/cart" element={<CartComp />} />
-          <Route path="/map" element={<MapComp />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <RouterProvider router={root} />;
 }
 
 export default App;
