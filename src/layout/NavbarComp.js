2c0d6c42ab08e345dc8d74564c1e2b830ab1e88d
N-jongsik <jongsik0625@naver.com>
2024-09-23 18:01:40 +0900

    refactor:navbar modify

--- src/layout/NavbarComp.js
+++ src/layout/NavbarComp.js
@@ -5,24 +5,6 @@ import { Router, Link } from "react-router-dom";
 function NavbarComp() {
   return (
     <nav>
-      {/* <h1>patagonia</h1>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/shop">Shop</NavLink>
-        </li>
-        <li>
-          <NavLink to="/detail">Detail</NavLink>
-        </li>
-        <li>
-          <NavLink to="/cart">Cart</NavLink>
-        </li>
-        <li>
-          <NavLink to="/map">Map</NavLink>
-        </li>
-      </ul> */}
       <div className="bg-black ">
         <div className="flex justify-around px-4">
           <div>&nbsp;</div>
@@ -32,11 +14,11 @@ function NavbarComp() {
                 className="font-bold h-full flex justify-center items-center text-center"
                 style={{ fontSize: "1.2rem", height: "4.8rem" }}
               >
-                추석 연휴 배송 안내
+                {/* 추석 연휴 배송 안내 */}
               </div>
             </h1>
           </nav>
-          <div className="hidden md:flex text-white ">
+          <div className="hidden md:flex text-white pr-40">
             <Link
               to={"/map"}
               className="font-bold flex justify-center items-center h-full text-center"
@@ -56,7 +38,7 @@ function NavbarComp() {
         </div>
         <nav>
           <div className="flex justify-around bg-white">
-            <div className="flex justify-center items-center">
+            <div className="hidden md:flex justify-center items-center">
               <Link to={"/"}>
                 <img
                   src={require("../assets/images/logo_patagonia.jpg")}
@@ -64,59 +46,77 @@ function NavbarComp() {
                 />
               </Link>
             </div>
-            <ul className="before_list flex justify-center items-center">
-              <li className="h-full relative flex justify-center items-center">
-                <a
-                  href="#"
-                  className="font-semibold hidden md:block py-2 text-black relative"
-                  style={{ padding: "0 3rem" }}
-                >
-                  <NavLink to="/shop">Shop</NavLink>
-                </a>
+            <ul className="flex justify-center items-center pl-2">
+              <li className="p-2 md:p-5 block md:hidden">
+                <i class="fa-solid fa-bars"></i>
               </li>
-              <li className="h-full relative flex justify-center items-center">
-                <a
-                  href="#"
-                  className="font-semibold hidden md:block py-2 text-black relative"
-                  style={{ padding: "0 3rem" }}
-                >
-                  <NavLink to="/detail">Detail</NavLink>
-                </a>
-              </li>
-              <li className="h-full relative flex justify-center items-center">
-                <a
-                  href="#"
-                  className="font-semibold hidden md:block py-2 text-black relative"
-                  style={{ padding: "0 3rem" }}
-                >
-                  <NavLink to="/cart">Cart</NavLink>
-                </a>
-              </li>
-              <li className="h-full relative flex justify-center items-center">
-                <a
-                  href="#"
-                  className="font-semibold hidden md:block py-2 text-black relative"
-                  style={{ padding: "0 3rem" }}
-                >
-                  <NavLink to="/map">Map</NavLink>
-                </a>
+              <li className="p-2 md:p-5 block md:hidden">
+                <i class="fa-solid fa-magnifying-glass"></i>
               </li>
             </ul>
-            <div className="text-black block py-2">
-              <ul className="flex justify-center items-center">
-                <li className="p-5 hidden md:block">
+            <ul className="before_list flex justify-center items-center pr-36">
+              <li className="h-full relative flex justify-center items-center">
+                <NavLink
+                  to={"/shop"}
+                  className="font-semibold hidden md:block py-2 text-black relative"
+                  style={{ padding: "0 3rem" }}
+                >
+                  Shop
+                </NavLink>
+              </li>
+              <li className="h-full relative flex justify-center items-center">
+                <NavLink
+                  to="/detail"
+                  className="font-semibold hidden md:block py-2 text-black relative"
+                  style={{ padding: "0 3rem" }}
+                >
+                  Detail
+                </NavLink>
+              </li>
+              <li className="h-full relative flex justify-center items-center">
+                <NavLink
+                  to="/cart"
+                  className="font-semibold hidden md:block py-2 text-black relative"
+                  style={{ padding: "0 3rem" }}
+                >
+                  Cart
+                </NavLink>
+              </li>
+              <li className="h-full relative flex justify-center items-center">
+                <NavLink
+                  to="/map"
+                  className="font-semibold hidden md:block py-2 text-black relative"
+                  style={{ padding: "0 3rem" }}
+                >
+                  Map
+                </NavLink>
+              </li>
+            </ul>
+            <div className="flex md:hidden justify-center items-center">
+              <Link to={"/"}>
+                <img
+                  src={require("../assets/images/logo_patagonia.jpg")}
+                  alt="logo_patagonia"
+                />
+              </Link>
+            </div>
+            <div className="text-black block py-2 pr-2 md:pr-8">
+              <ul className="flex justify-center items-center cursor-pointer">
+                <li className="p-2 md:p-5 hidden md:block">
                   <i class="fa-solid fa-magnifying-glass"></i>
                 </li>
-                <li className="p-5">
+                <li className="p-2 md:p-5">
                   <i class="fa-regular fa-user"></i>
                 </li>
-                <li className="p-5">
+                <li className="p-2 md:p-5">
                   <i class="fa-regular fa-heart"></i>
                 </li>
-                <li className="p-5">
-                  <i class="fa-solid fa-cart-shopping"></i>
+                <li className="p-2 md:p-5">
+                  <Link to={"/cart"}>
+                    <i class="fa-solid fa-cart-shopping"></i>
+                  </Link>
                 </li>
-                <li className="p-5 hidden md:block">
+                <li className="p-2 md:p-5 hidden md:block">
                   <i class="fa-solid fa-bars"></i>
                 </li>
               </ul>
